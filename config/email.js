0c620b019ec5b0f1f956484395f204439a0009f5
jijamjk033 <jijamjk333@gmail.com>
2024-01-03 12:07:26 +0530

    NodeMailer

--- config/email.js
+++ config/email.js
@@ -1,39 +1,36 @@
 const nodemailer = require('nodemailer');
-require("dotenv").config();
+require('dotenv').config()
 
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.USER,
+    pass: process.env.PASSWORD,
+  },
+});
 
-const sendVarifyMail = async (email, req) => {
-  try {
+const sendVarifyMail = async (email,req) => {
 
-    const otp = generateOTP(4);
+  const otp = generateOTP(4)
+  req.session.otp = otp
+  const mailOptions = {
+    from: process.env.USER,
+    to: email,
+    subject: 'Your OTP Code for verification',
+    text: `Your OTP code is: ${otp}`,
+  };
 
-    req.session.otp = otp;
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error('Error sending email:', error);
+    } else {
+      console.log('Email sent:', info.response);
+      console.log('OTP:', otp);
+    }
+  });
+  return otp
+}
 
-    console.log(req.session.otp, "otp")
-    const transporter = nodemailer.createTransport({
-
-      host: 'smtp.gmail.com',
-      port: 587,
-      secure: false,
-      requireTLS: true,
-      auth: {
-        user: `${process.env.USER}`,
-        pass: `${process.env.PASSWORD}`,
-      },
-    });
-    const mailOptions = {
-      from: `${process.env.USER}`,
-      to: email,
-      subject: 'For verification purpose',
-      html: `<p>Hello,\n Welcome to Femme Fatale.\nHere is the OTP for your registration : <strong>${otp}</strong>. Enter this to verify your Email. Please do not share this password with anyone.</p>`,
-    };
-    const information = await transporter.sendMail(mailOptions);
-    console.log(information.messageId);
-  } catch (error) {
-    console.log(error);
-  }
-
-};
 
 function generateOTP(length) {
   const characters = '0123456789'; // The characters to use for the OTP
