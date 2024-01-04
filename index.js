3ec58f1b8a21e8caaa8b15d460cb907a28ab9d8c
jijamjk033 <jijamjk333@gmail.com>
2024-01-04 20:45:06 +0530

    made mobile responsive

--- index.js
+++ index.js
@@ -40,6 +40,11 @@ app.use('/',userRoute);
 const adminRoute = require("./routes/adminRoute");
 app.use('/admin',adminRoute);
 
+app.use((req, res, next) => {
+    res.status(404).render("./layouts/404error", { userData: null });
+  });
+  
+
 app.listen(3030, ()=>{
     console.log("Server is running on port:  http://localhost:3030");
 });
